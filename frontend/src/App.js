68dd34eebff1b3051402c85728801c15dd76117e
rose-jpg <rhouzmeri19@gmail.com>
2022-10-07 11:09:09 +0100

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Loader from './components/Loader'
+import Counter from './components/Counter'
+import About from './components/About'
+
+function App() {
+  const [loader, setLoader] = useState(true)
+  useEffect(()=>{
+     setTimeout(()=>{
+       setLoader(false)
+     },1000)
+  },[])
+  return loader ? (
+    <Loader/>
+  ):
+  (
+    <div>
+      
+     <Hero/>
+     <Counter/>
+     <About/>
+
+     
+    </div>
+  )
+}
+
+export default App
+
