6d6ebb821de0a8e03719b2b180b96264279cd1f6
rose-jpg <rhouzmeri19@gmail.com>
2022-10-07 11:32:56 +0100

    commit

---
+++ frontend/src/components/About.js
@@ -0,0 +1,91 @@
+import React from "react"
+
+
+
+function About() {
+  return (
+    <>
+      <div className='min-h-[400px] gri ml-6 mr-6'>
+        <div>
+          <h1 className="flex mt-[150px] justify-center
+           text-[10rem]  text-[#91FFFF]">01</h1>
+           
+           
+        </div>
+        <div className="self-center w-[500px]">
+          <h5>ABOUT FINROSE</h5>
+          <h1 className="text-2xl font-extrabold tracking-[0.2rem] mt-2">WE HELP GET YOUR MONEY<br></br> BACK IN ONE TAP</h1>
+          <p className="mt-3">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor
+            aliquid officiis accusantium deleniti iure possimus, in ducimus
+            aliquid officiis accusantium deleniti iure possimus, in ducimus
+            aliquid officiis accusantium deleniti iure possimus, in ducimus
+            
+          </p>
+        </div>
+      </div>
+      <div className='min-h-[400px] gri ml-6'>
+       
+        <div className="self-center w-[500px]">
+          <h5>ABOUT FINROSE</h5>
+          <h1>WE HELP GET YOUR MONEY BACK</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor
+            aliquid officiis accusantium deleniti iure possimus, in ducimus
+            suscipit ab corporis dicta consequatur repelle ndus voluptates at
+            sed nesciunt ipsum voluptatem odio velit, rerum sit. Esse ducimus
+            libero cum! Tempora ab repellendus, quibusdam, neque odit atque quis
+            iste sit, error eius quo.
+          </p>
+        </div>
+        <div>
+          <h1 className="flex justify-center
+           text-[10rem]  text-[#91FFFF]">02</h1>
+           
+        </div>
+      </div>
+      <div className='min-h-[600px] gri ml-6'>
+      <div>
+          <h1 className="flex justify-center
+           text-[10rem]  text-[#91FFFF]">03</h1>
+           
+        </div>
+        <div className="self-center w-[500px]">
+          <h5>ABOUT FINROSE</h5>
+          <h1>WE HELP GET YOUR MONEY BACK</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor
+            aliquid officiis accusantium deleniti iure possimus, in ducimus
+            suscipit ab corporis dicta consequatur repelle ndus voluptates at
+            sed nesciunt ipsum voluptatem odio velit, rerum sit. Esse ducimus
+            libero cum! Tempora ab repellendus, quibusdam, neque odit atque quis
+            iste sit, error eius quo.
+          </p>
+        </div>
+       
+      </div>
+      <div className='min-h-[400px] gri ml-6'>
+       
+        <div className="self-center w-[500px]">
+          <h5>ABOUT FINROSE</h5>
+          <h1>WE HELP GET YOUR MONEY BACK</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor
+            aliquid officiis accusantium deleniti iure possimus, in ducimus
+            suscipit ab corporis dicta consequatur repelle ndus voluptates at
+            sed nesciunt ipsum voluptatem odio velit, rerum sit. Esse ducimus
+            libero cum! Tempora ab repellendus, quibusdam, neque odit atque quis
+            iste sit, error eius quo.
+          </p>
+        </div>
+        <div>
+          <h1 className="flex justify-center
+           text-[10rem]  text-[#91FFFF]">04</h1>
+           
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default About
---
+++ frontend/src/components/Counter.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react"
+import Countup from "react-countup"
+import ScrollTriger from "react-scroll-trigger"
+
+function Counter() {
+  const [counterOn, setCounterOn] = useState(false)
+  return (
+    <>
+      <ScrollTriger className="bg-[#040910] text-white"
+        onEnter={() => setCounterOn(true)}
+        onExit={() => setCounterOn(false)}
+      >
+        <div className=' ml-6 flex w-[94vw] justify-between py-[50px] px-[20px] '>
+          <div>
+            <h3 className="text-3xl">
+              {counterOn && (
+                <Countup start={0} end={100} duration={2} delay={0} />
+              )}
+              +
+            </h3>
+            <p className="text-xl">Banks Affliation</p>
+          </div>
+          <div>
+            <h3 className="text-3xl">
+              {counterOn && (
+                <Countup start={0} end={87} duration={2} delay={0} />
+              )}
+              %
+            </h3>
+            <p className="text-xl">Sucess Rate</p>
+          </div>
+          <div>
+            <h3 className="text-3xl">
+              {counterOn && (
+                <Countup start={2020} end={2022} duration={2} delay={0} />
+              )}
+              +
+            </h3>
+            Up Till Date
+          </div>
+          <div>
+            <h3 className="text-3xl">
+              {counterOn && (
+                <Countup start={0} end={100} duration={2} delay={0} />
+              )}
+              %
+            </h3>
+            <p className="text-xl">Cash Back Guaranteed</p>
+          </div>
+          
+         
+        </div>
+      </ScrollTriger>
+    </>
+  )
+}
+
+export default Counter
